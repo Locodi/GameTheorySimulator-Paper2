08506029e2bb2528b8cf81038ea9c572a27ddeca
Locodi <Locodi@users.noreply.github.com>
2021-12-21 18:47:13 +0000

    Update AdvanceSimulator_MainMenuControler.java

--- Java/src/application/AdvanceSimulator_MainMenuControler.java
+++ Java/src/application/AdvanceSimulator_MainMenuControler.java
@@ -61,27 +61,7 @@ public class AdvanceSimulator_MainMenuControler {
 			
 	}
 	
-	public void goGrowthSimulation(ActionEvent event)throws IOException
-	{
-				
-		// change window
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("AdvanceSimulator_GrowthSimulation.fxml"));
-		Parent temp_parent = (Parent)loader.load();
-		Scene temp_scene = new Scene(temp_parent);
-		Stage app_Stage = (Stage)((Node) event.getSource()).getScene().getWindow();
-		//keep the current window size
-		double stage_height = app_Stage.getHeight();
-		double stage_width = app_Stage.getWidth();
 		
-		// change scene
-		app_Stage.setScene(temp_scene);
-		
-		// set window size to previous scene
-		app_Stage.setHeight(stage_height);
-		app_Stage.setWidth(stage_width);				
-				
-			
-	}	
 	
 	
 }
